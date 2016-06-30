6f16d00a74e7993fcb23972225294816e9d3224b
Alexey Korepanov <alexkorep@gmail.com>
2016-06-30 21:51:24 +0200

    added package.json and webpack config

---
+++ src/js/index.jsx
@@ -0,0 +1,12 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import Voting from './components/Voting';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <div>Welcome!</div>,
+  document.getElementById('app')
+);
